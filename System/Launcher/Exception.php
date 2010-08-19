ce97bc47111a4f12569f48e339629ec44ea1abaf
olleolleolle <olle.jonsson@gmail.com>
2010-08-19 19:25:20 +0200

    Added tags to docblock.

--- System/Launcher/Exception.php
+++ System/Launcher/Exception.php
@@ -18,6 +18,14 @@ require_once 'PEAR/Exception.php';
 
 /**
  * Custom exception.
+ *
+ * @category System
+ * @package  System_Launcher
+ * @author   Christian Weiske <cweiske@php.net>
+ * @author   Olle Jonsson <olle.jonsson@gmail.com>
+ * @license  http://www.gnu.org/licenses/lgpl.html LGPL
+ * @link     http://github.com/olleolleolle/File_Launcher
+ * @since    File available since Release 0.1.0
  */
 class System_Launcher_Exception extends PEAR_Exception {
 }
